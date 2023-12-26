44ba774aa5deb75e3d3d724d013eebb72a606857
Eyob Mamo <eyobmamo25@gmail.com>
2023-12-26 16:59:49 -0200

    Added solution for Sort the People

--- week1/sort-the-people.py
+++ week1/sort-the-people.py
@@ -1,10 +1,28 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(len(names)):
-            for j in range(i+1,len(names)):
-                if heights[i]<heights[j]:
-                    heights[i],heights[j]=heights[j],heights[i]
-                    names[i],names[j]=names[j],names[i]
+        nh_zip=zip(heights,names)
+        for i in range(len(heights)):
+            for j in range(len(heights)-i-1):
+                if  heights[j]<heights[j+1]:
+                    heights[j],heights[j+1]=heights[j+1],heights[j]
+                    names[j],names[j+1]=names[j+1],names[j]
         return names            
+                    
+
+        
+            
+         
+
+
+
+
+                
+
+
+
+
+
+
+                        
 
         
